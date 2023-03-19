7fa85b37127e0251a1bdb24c44caeeb50efdc905
Kit <nvmmonkey@github.com>
2023-03-19 21:43:57 +0800

    add .md fetching SR building the featuredpage/allpostspage, fix error in utils

--- pages/index.js
+++ pages/index.js
@@ -2,49 +2,26 @@ import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-    date: "2022-01-01",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-    date: "2022-01-01",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-    date: "2022-01-01",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-    date: "2022-01-01",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    // revalidate: 1800,
+  };
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,43 +1,19 @@
-import { DUMMY_POSTS } from "..";
 import AllPosts from "../../components/home-page/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-function AllPostsPage() {
-  const DUMMY_POSTS = [
-    {
-      slug: "getting-started-with-nextjs",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-      date: "2022-01-01",
-    },
-    {
-      slug: "getting-started-with-nextjs2",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-      date: "2022-01-01",
-    },
-    {
-      slug: "getting-started-with-nextjs3",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-      date: "2022-01-01",
-    },
-    {
-      slug: "getting-started-with-nextjs4",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for production, it makes building full-stack React apps and sites a breeze and ship with built-in SSR.",
-      date: "2022-01-01",
-    },
-  ];
+function AllPostsPage(props) {
+  return <AllPosts posts={props.posts} />;
+}
 
-  return <AllPosts posts={DUMMY_POSTS} />;
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+    // revalidated: 600,
+  };
 }
 
 export default AllPostsPage;
